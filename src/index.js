872b39869c9143defcb9415105fa6247399ba770
unknown <edwardachike@gmail.com>
2022-06-03 06:38:22 -0700

    pupulating html from javascript add function working

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 import './index.css';
 import TaskList from './module/TaskList';
-import { DateTime } from './module/luxton.js';
+import { DateTime } from './module/luxton';
 
 const placeHolder = document.querySelector('#place-holder');
 const form = document.querySelector('form');
 const tasklist = new TaskList(placeHolder);
-const  checkboxes = document.querySelectorAll("input[type=checkbox]");
-let getDate = DateTime.now();
-document.querySelector('.footer-year').innerHTML = `${getDate.year}`
+const checkboxes = document.querySelectorAll('input[type=checkbox]');
+const getDate = DateTime.now();
+document.querySelector('.footer-year').innerHTML = `${getDate.year}`;
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    const object = {
-        iscompleted: false,
-        description: '',
-        index: null
-    }
-    object.description = document.querySelector('.desc').value;
-    if (object.description==='') {
-        return;
-      }
-      document.querySelector('.desc').value = '';
-      tasklist.addtask(object, placeHolder);
-     
-})
-
-checkboxes.forEach((checkbox)=>checkbox.addEventListener('change', tasklist.isDone.bind(tasklist)))
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const object = {
+    iscompleted: false,
+    description: '',
+    index: null,
+  };
+  object.description = document.querySelector('.desc').value;
+  if (object.description === '') {
+    return;
+  };
+  document.querySelector('.desc').value = '';
+  tasklist.addtask(object, placeHolder);
+});
+checkboxes.forEach((checkbox) => checkbox.addEventListener('change', tasklist.isDone.bind(tasklist)));
 
